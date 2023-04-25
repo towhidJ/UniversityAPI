5f4c517fe470e3a96bca445822fddaecf2f857d1
towhidJ <towhidulislamcse@gmail.com>
2023-04-25 20:29:02 +0600

    updated student result add

--- UniversityAPI/Repository/StudentResultRepository.cs
+++ UniversityAPI/Repository/StudentResultRepository.cs
@@ -22,6 +22,13 @@ namespace UniversityAPI.Repository
                     return "This subject Result already entry";
                 }
 
+                var c = _db.EnrollCourseTb.FirstOrDefault(s =>
+                    s.StudentId == studentResult.StudentId && s.CourseId == studentResult.CourseId);
+                if (c == null)
+                {
+                    return "This Student Course not Enrolled";
+                }
+                
                 await DbSet.AddAsync(studentResult);
                 return "Result Add Successful";
 
@@ -37,25 +44,6 @@ namespace UniversityAPI.Repository
         public List<StudentResultViewModel> GetAllStudentResults(int studentId)
         {
             
-
-            // var result = (from ec in _db.EnrollCourseTb
-            //         join studentResult in _db.StudentResultTb on ec.StudentId equals studentResult.StudentId into stdEc
-            //         from stdResult in stdEc.DefaultIfEmpty()
-            //         join gradeLetter in _db.GradeLetterTb on stdResult.GradeLetterId equals gradeLetter.Id into
-            //             grdLetter
-            //         from gradeLetter in grdLetter.DefaultIfEmpty()
-            //               join std in _db.StudentTb on ec.StudentId equals std.Id 
-            //         join course in _db.CourseTb on ec.CourseId equals course.Id
-            //         where ec.StudentId== studentId
-            //         select new StudentResultView()
-            //         {
-            //             CourseId = course.Id,
-            //             CourseCode = course.CourseCode, 
-            //             CourseName = course.CourseName,
-            //             Grade = gradeLetter == null? "Not Graded Yet" : gradeLetter.GradeLetterMarkes,
-            //             Credit = course.Credit
-            //         })
-            //     .ToList();
 
             var result = _db.StudentResultViews.FromSqlRaw($"Student_Result {studentId}" );
 
